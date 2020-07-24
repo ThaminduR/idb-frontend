3923d3892f21e2f5b88e2988d4714d46f0e97181
MithunW <mithun.17@cse.mrt.ac.lk>
2020-07-25 00:18:10 +0530

    constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://ec2-54-254-76-188.ap-southeast-1.compute.amazonaws.com/api";
-// export const API_BASE_URL = "http://localhost:4000";
+export const API_BASE_URL = "http://localhost:4000/api";
+// export const API_BASE_URL = "http://localhost:4000"http://ec2-54-254-76-188.ap-southeast-1.compute.amazonaws.com/api;
 export const SITE_NAME = 'Template Site';
 export const DEV_NAME = 'Test'
