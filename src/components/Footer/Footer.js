9cc9f38f4527be9cf9bb072a553ce03dad0e6aea
ThaminduR <thamindu.randil@gmail.com>
2020-07-27 12:42:47 +0530

    developed by removed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import { DEV_NAME } from '../../constants/constants';
 function Footer(props) {
     return (
         <footer className='float-bottom'>
-            <p className='footer-text'>Industrial Development Board &copy; {new Date().getFullYear()} | Developed By {DEV_NAME}</p>
+            <p className='footer-text'>Industrial Development Board &copy; {new Date().getFullYear()}</p>
         </footer>
     )
 }
